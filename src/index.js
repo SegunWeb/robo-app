9768ead4e6b319edeba1be16bec4569f6254c4bd
Sergey <mrsegun2014@gmail.com>
2019-11-29 15:03:09 +0200

    add redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import './index.css';
 import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+
+    <App />,
+    document.getElementById('root'));
